0d404a1612e50a5937044f91c8aec057e8f80b86
Lior Bar-On <baronlior@gmail.com>
2018-05-06 13:26:34 +0300

    impl

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,11 +1,45 @@
 
-object Hello{
+val priceMap = mapOf(
+  1 to 1.0,
+  2 to 0.95,
+  3 to 0.9,
+  4 to 0.80,
+  5 to 0.75
+)
 
-  fun act() {
-    println("Hello World")
-  }
+
+
+fun price(books : List<Int>) : Double {
+  if (books.isEmpty())
+    return 0.0
+
+  val buckets = books.groupBy {it }
+  val thisPrice = buckets.size *8.0 * priceMap[buckets.size]!!
+//  val rest_price = price(buckets.flatMap { it.value.drop(1)})
+
+
+
+  val rest_price = priceOf(books)
+  return rest_price
 }
 
-fun main(args: Array<String>) {
-  Hello.act()
+
+fun priceOf(books : List<Int>) : Double {
+  if (books.isEmpty())
+    return 0.0
+
+  val uniqueBooksNumber = books.groupBy {it }.size
+  val otherBooksCount = books.size - uniqueBooksNumber
+  return priceMap[uniqueBooksNumber]!! * uniqueBooksNumber * 8.0+  otherBooksCount *8.0
 }
+
+fun priceless(books : List<Int>) : Double {
+  if (books.isEmpty())
+    return 0.0
+
+  val buckets = books.groupBy {it }
+  val thisPrice = buckets.size *8.0 * priceMap[buckets.size]!!
+  val rest_price = priceless(buckets.flatMap { it.value.drop(1)})
+  return thisPrice + rest_price
+}
+
--- src/test/kotlin/HelloTest.kt
+++ src/test/kotlin/HelloTest.kt
@@ -3,12 +3,32 @@ import org.junit.jupiter.api.Test
 
 internal class HelloTest {
 
-  @Test fun foo() {
-    expect(2+2).toBe(4)
+  @Test fun basic() {
+    expect(price(emptyList())).toBe(0.0)
+    (0..4).forEach {
+      expect(price(listOf(it))).toBe(8.0)
+    }
+    expect(price(listOf(0, 0))).toBe(8.0 * 2)
+    expect(price(listOf(1, 1, 1))).toBe(8.0 * 3)
   }
 
 
-  @Test fun foo2() {
-    expect(2+4).toBe(6)
+  @Test fun simpleCombination() {
+    expect(price(listOf(0, 1))).toBe(8.0 * 2 * 0.95)
+    expect(price(listOf(2, 0, 4))).toBe(8.0 * 3 * 0.90)
+    expect(price(listOf(0,1,2,4))).toBe(8.0 * 4 * 0.80)
+    expect(price(listOf(0,1,2,3,4))).toBe(8.0 * 5 * 0.75)
   }
+
+  @Test fun testSeveralDiscounts() {
+    expect(price(listOf(0, 0, 1))).toBe(8 + (8 * 2 * 0.95))
+    expect(price(listOf(0, 0, 1, 1))).toBe(2 * (8 * 2 * 0.95))
+    expect(price(listOf(0, 0, 1, 2, 2, 3))).toBe((8 * 4 * 0.8) + (8 * 2 * 0.95))
+    expect(price(listOf(0, 1, 1, 2, 3, 4))).toBe(8 + (8 * 5 * 0.75))
+  }
+
+  @Test fun testEdgeCases() {
+    expect(price(listOf(0, 0, 1, 1, 2, 2, 3, 4))).toBe(2 * (8 * 4 * 0.8))
+  }
+
 }
